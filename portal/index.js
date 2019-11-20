0f633489c9b0a3c6ba682f94d4cc467cc73c0f0d
Jordy Bulten <jordy.bulten@gmail.com>
2019-11-20 16:57:24 +0100

    fix: add basename

--- portal/index.js
+++ portal/index.js
@@ -10,7 +10,7 @@ import App from './App';
 import './index.scss';
 
 ReactDOM.render((
-  <Router> 
+  <Router basename={'/kpn-style'}>
     <App />
   </Router>
 ), document.getElementById('kpn-style-portal'));
